2ddcb77b1cf1e6b38841435bfe162be4e20a1bc4
Theowyn <smurfacckekw@gmail.com>
2023-12-08 20:16:45 +0100

    attempt at fixing scrolltrigger + lazyload

--- source/_static/scripts/lazyloadVideos.js
+++ source/_static/scripts/lazyloadVideos.js
@@ -1,25 +1,30 @@
 // load and start lozad
-const observer = lozad();
+const observer = lozad('.lozad', {
+  loaded: function(el) {
+    // Custom callback on successful load
+    el.classList.add('lozad'); // You can add a class for fading in the loaded content
+  }
+});
 observer.observe();
 
-// load and configure ScrollTrigger
-gsap.registerPlugin(ScrollTrigger);
+document.addEventListener('DOMContentLoaded', function() {
+  // load and configure ScrollTrigger
+  gsap.registerPlugin(ScrollTrigger);
 
-let allVideoDivs = gsap.utils.toArray('.wrapper');
+  let allVideoDivs = gsap.utils.toArray('.wrapper');
 
-allVideoDivs.forEach((videoDiv, i) => {
-  
-  let videoElem = videoDiv.querySelector('video')
-  
-  ScrollTrigger.create({
-    trigger: videoElem,
-    start: 'top 80%',
-    end: 'top -40%',
-    markers: false,
-    onEnter: () => videoElem.play(),
-    onEnterBack: () => videoElem.play(),
-    onLeave: () => videoElem.pause(),
-    onLeaveBack: () => videoElem.pause(),
+  allVideoDivs.forEach((videoDiv, i) => {
+    let videoElem = videoDiv.querySelector('video');
+
+    ScrollTrigger.create({
+      trigger: videoElem,
+      start: 'top top',
+      end: '+=500',
+      markers: false,
+      onEnter: () => videoElem.play(),
+      onEnterBack: () => videoElem.play(),
+      onLeave: () => videoElem.pause(),
+      onLeaveBack: () => videoElem.pause(),
+    });
   });
-  
 });
