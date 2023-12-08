cef8928314cfac713386648addcb8d9c65b690aa
Theowyn <smurfacckekw@gmail.com>
2023-12-08 20:35:14 +0100

    yet another attempt at fixing lazyload

--- source/_static/scripts/lazyloadVideos.js
+++ source/_static/scripts/lazyloadVideos.js
@@ -1,8 +1,26 @@
-// load and start lozad
 const observer = lozad('.lozad', {
-  loaded: function(el) {
-    // Custom callback on successful load
-    el.classList.add('lozad'); // You can add a class for fading in the loaded content
-  }
+  rootMargin: '10px 0px', // syntax similar to that of CSS Margin
+  threshold: 0.1, // ratio of element convergence
+  enableAutoReload: true // it will reload the new image when validating attributes changes
 });
 observer.observe();
+
+// load and configure ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+let allVideoDivs = gsap.utils.toArray('.wrapper');
+
+allVideoDivs.forEach((videoDiv, i) => {
+  let videoElem = videoDiv.querySelector('video');
+
+  ScrollTrigger.create({
+    trigger: videoElem,
+    start: 'top top',
+    end: '+=500',
+    markers: false,
+    onEnter: () => videoElem.play(),
+    onEnterBack: () => videoElem.play(),
+    onLeave: () => videoElem.pause(),
+    onLeaveBack: () => videoElem.pause(),
+  });
+});
