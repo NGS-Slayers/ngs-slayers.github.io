f14c70c23413afc5a3a8b235f44cd4b1f6cb00c5
Theowyn <smurfacckekw@gmail.com>
2024-04-02 17:08:05 +0200

    add play promise for autoplay media

--- source/_static/scripts/autoplay.js
+++ source/_static/scripts/autoplay.js
@@ -22,13 +22,36 @@ document.addEventListener("DOMContentLoaded", function () {
                 // Delay autoplay if the page is loaded via anchor link
                 setTimeout(() => {
                     if (!entry.target.hasAttribute('data-playing') && entry.target.hasAttribute('data-can-play')) {
-                        entry.target.play();
-                        entry.target.setAttribute('playing', 'true');
+                        playMedia(entry.target)
+                            .then(() => {
+                                entry.target.setAttribute('playing', 'true');
+                            })
+                            .catch(error => {
+                                console.error('Failed to play media:', error);
+                            });
                     }
                 }, 200);
             } else {
                 entry.target.pause();
                 entry.target.setAttribute('paused', 'true');
+            }
+        });
+    }
+
+    // Function to play media with a promise-based API
+    function playMedia(mediaElement) {
+        return new Promise((resolve, reject) => {
+            const playPromise = mediaElement.play();
+            if (playPromise !== undefined) {
+                playPromise
+                    .then(() => {
+                        resolve();
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            } else {
+                resolve(); // For browsers that do not return a promise
             }
         });
     }
